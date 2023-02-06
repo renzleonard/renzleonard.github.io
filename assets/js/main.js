422d1b82b380688053040e8542a8bb68255ae9cd
renzleonard <113642364+renzleonard@users.noreply.github.com>
2023-02-06 20:41:31 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -141,12 +141,11 @@
 						$menu._hide();
 
 			});
-
+			
 			document.getElementById("scrollTopBtn").addEventListener("click", function() {
 				window.scrollTo({
 				  top: 0,
 				  behavior: "smooth"
 				});
 			  });
-			  
 })(jQuery);
